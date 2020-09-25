94f20dcba7b9402ded9889c2ada9968e5e6dd136
Lucas Silveira <lugano1995@gmail.com>
2020-09-24 21:08:44 -0300

    feat: add Layout component into About component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 // import { Container } from './styles';
 
 const AboutPage = () => (
-  <>
+  <Layout>
+    <SEO title="About" />
     <h1>About Page</h1>
     <ul>
         <li>
@@ -14,7 +17,7 @@ const AboutPage = () => (
           <Link to="/about" activeStyle={{ color: 'red' }}>About</Link>
         </li>
       </ul>
-  </>
+  </Layout>
 )
 
 export default AboutPage
