1388ce064b5aa9b0a3675ffd3a65a28ff5848dcf
Lucas Silveira <lugano1995@gmail.com>
2020-09-24 21:03:05 -0300

    feat: add menu

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 // import { Container } from './styles';
 
 const AboutPage = () => (
-  <h1>About Page</h1>
+  <>
+    <h1>About Page</h1>
+    <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about" activeStyle={{ color: 'red' }}>About</Link>
+        </li>
+      </ul>
+  </>
 )
 
 export default AboutPage
